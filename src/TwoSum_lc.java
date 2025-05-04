bf67d5ae3f9ca00d5c4ac4e808ad1419f4960691
Anand0047 <anandram221003@gmail.com>
2025-05-04 23:43:40 +0530

    INITIAL COMMIT

---
+++ src/TwoSum_lc.java
@@ -0,0 +1,17 @@
+public class TwoSum_lc {
+    public static void main(String[] args) {
+        int num[] = {2,7,11,15};
+        int target = 9;
+        int indx1 = 0;
+        int indx2 = 0;
+        for (int i = 0;i<num.length;i++){
+            for(int k=0;k<num.length;k++){
+                if (num[i]+num[k]==target){
+                    indx1 =i;
+                    indx2 =k;
+                }
+            }
+        }
+
+    }
+}
