65b0d8376068ecfc842d375be81deb78c69c542d
Anand0047 <anandram221003@gmail.com>
2025-05-11 22:38:55 +0530

    INITIAL COMMIT

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class InsertionSort {
     public static void main(String[] args) {
 
-        int arr[] = {7,3,5,2,6,8,2,1};
+        int[] arr = {7,3,5,2,6,8,2,1};
 
         for (int i =1;i<arr.length;i++){
             int key = arr[i];
