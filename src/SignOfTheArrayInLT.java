0514c247e16674a7c1c1eef4373926c8840c0c71
Anand0047 <anandram221003@gmail.com>
2025-05-16 23:08:37 +0530

    INITIAL COMMIT

---
+++ src/SignOfTheArrayInLT.java
@@ -0,0 +1,22 @@
+public class SignOfTheArrayInLT {
+    public static void main(String[] args) {
+
+        int [] nums = {-1,-2,-3,-4,3,2,1};
+        int x =1;
+        for (int i : nums){
+            x*=i;
+        }
+        System.out.println(signFunc(x));
+    }
+    static int signFunc(int x){
+        if (x>0){
+            return 1;
+        }else if (x<0){
+            return -1;
+        }else {
+            return 0;
+        }
+
+
+    }
+}
