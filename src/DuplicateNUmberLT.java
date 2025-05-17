4fa0cae47f2c080b8fbc27429b7410041c40b7a2
Anand0047 <anandram221003@gmail.com>
2025-05-17 22:31:20 +0530

    INITIAL COMMIT

--- src/DuplicateNUmberLT.java
+++ src/DuplicateNUmberLT.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class DuplicateNUmberLT {
     public static void main(String[] args) {
+
         int [] nums = {4,3,2,7,8,2,3,1};
         System.out.println(findDuplicates(nums));
     }
