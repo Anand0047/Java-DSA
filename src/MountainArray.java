c481eb24845aa71298d70c3eb4dd04430a6447da
Anand0047 <anandram221003@gmail.com>
2025-05-20 15:54:05 +0530

    INITIAL COMMIT

--- src/MountainArray.java
+++ src/MountainArray.java
@@ -19,21 +19,41 @@ public class MountainArray {
 //        }
         }
 
-    public int peakIndexInMountainArray(int[] mountainArray) {
+//    public int peakIndexInMountainArray(int[] mountainArray) {
+//
+//         int start = 0;
+//         int end = mountainArray.length-1;
+//
+//         while (start<end){
+//             int mid = start + (end -start) /2;
+//             int target;
+//             if (mountainArray[mid] > mountainArray[mid+1]){
+//                 end = mid ;
+//             }else if (mountainArray[mid] < mountainArray[mid+1]){
+//                 start = mid +1;
+//             }else if (target == mountainArray[mid]){
+//                 return start;
+//             }
+//         }
+//         return -1;
+//    }
 
-         int start = 0;
-         int end = mountainArray.length-1;
-
-         while (start<end){
-             int mid = start + (end -start) /2;
-             if (mountainArray[mid] > mountainArray[mid+1]){
-                 end = mid ;
-             }else {
-                 start = mid +1;
-             }
-         }
-         return start;
-    }
-
+//    public int findInMountainArray(int target, MountainArray mountainArr) {
+//        int start = 0;
+//        int end = mountainArr.length-1;
+//
+//        while (start<end){
+//            int mid = start + (end -start) /2;
+//            int target;
+//            if (mountainArr[mid] > mountainArr[mid+1]){
+//                end = mid ;
+//            }else if (mountainArr[mid] < mountainArr[mid+1]){
+//                start = mid +1;
+//            }else if (target == mountainArr[mid]){
+//                return start;
+//            }
+//        }
+//        return -1;
+//    }
     }
 
---
+++ src/PowerXN_Leetcode.java
@@ -0,0 +1,26 @@
+public class PowerXN_Leetcode {
+    public static void main(String[] args) {
+        double x = 2.00000;
+        int  n = 10;
+        myPow(x,n);
+
+    }
+    public static double myPow(double x, int n) {
+        long power = n;
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+
+        double result = 1.0;
+        while (power > 0) {
+            if (power % 2 == 1) {
+                result *= x;
+            }
+            x *= x;
+            power /= 2;
+        }
+
+        return result;
+    }
+}
