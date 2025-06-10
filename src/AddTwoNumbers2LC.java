dabaa0ed2e759116b5ad792907c039ceeaa81857
Anand0047 <anandram221003@gmail.com>
2025-06-10 23:43:05 +0530

    INITIAL COMMIT

---
+++ src/AddTwoNumbers2LC.java
@@ -0,0 +1,41 @@
+public class AddTwoNumbers2LC {
+    public static void main(String[] args) {
+//        String a = "2";
+//        String b = "4";
+//        String c = a.concat(b);
+//        System.out.println(c);
+//        int d = Integer.parseInt(c);
+//        System.out.println(d*2);
+
+    }
+//    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//        ListNode dummy = new ListNode(0);
+//        ListNode ans = dummy;
+//        int carry = 0;
+//
+//        while(l1 != null || l2 != null || carry != 0){
+//
+//            int x = (l1 != null) ? l1.val : 0;
+//            int y = (l2 != null) ? l2.val : 0;
+//            int sum = carry+ x+y;
+//
+//            carry = sum/10;
+//
+//            ans.next = new ListNode(sum%10);
+//            ans = ans.next;
+//
+//            if(l1 != null){
+//                l1 = l1.next;
+//            }
+//            if(l2 != null){
+//                l2 = l2.next;
+//            }
+//
+//        }
+//
+//        return dummy.next;
+
+//    }
+//}
+
+}
--- src/HashImpEx.java
+++ src/HashImpEx.java
@@ -1,2 +1,8 @@
 public class HashImpEx {
+    public static void main(String[] args) {
+        int[] givenArr = {1,3,2,1,3};
+
+        int[] hash;
+
+    }
 }
