0e1979434823a2fb74a7ca186b1a6eb97812a64d
Anand0047 <anandram221003@gmail.com>
2025-06-15 00:54:47 +0530

    INITIAL COMMIT

---
+++ src/BinaryTreeImp.java
@@ -0,0 +1,22 @@
+class BinaryTreeImp {
+
+    public BinaryTreeImp() {
+
+    }
+    private Node root;
+
+
+
+
+
+    
+    private static class Node{
+        int value;
+        Node left;
+        Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+}
