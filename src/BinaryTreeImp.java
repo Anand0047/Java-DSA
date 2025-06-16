0a13e0e0d7c365662db4ad6291f270906f5c3784
Anand0047 <anandram221003@gmail.com>
2025-06-16 23:28:06 +0530

    INITIAL COMMIT

--- src/BinaryTreeImp.java
+++ src/BinaryTreeImp.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.Stack;
+
 class BinaryTreeImp {
 
     public BinaryTreeImp() {
@@ -19,4 +22,32 @@ class BinaryTreeImp {
             this.value = value;
         }
     }
+
+
+    public void populate(Scanner sc ){
+        int val = sc.nextInt();
+        root = new Node(val);
+        populate(sc,root);
+    }
+
+    private void populate(Scanner sc, Node node) {
+        System.out.println("Do you want to enter left of " +  node.value);
+        Boolean left = sc.nextBoolean();
+        if (left){
+            System.out.println("Enter the value of the left of " + node.value);
+            int val = sc.nextInt();
+            node.left = new Node(val);
+            populate(sc,node.left);
+        }
+
+        System.out.println("Do you want to enter left of " +  node.value);
+        Boolean right = sc.nextBoolean();
+        if (right){
+            System.out.println("Enter the value of the right of " + node.value);
+            int val = sc.nextInt();
+            node.right = new Node(val);
+            populate(sc,node.right);
+        }
+    }
+
 }
