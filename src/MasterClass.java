8268358816e962fbb78bc9ad9767841b95609d43
Anand0047 <anandram221003@gmail.com>
2025-06-18 00:02:54 +0530

    INITIAL COMMIT

--- src/MasterClass.java
+++ src/MasterClass.java
@@ -5,8 +5,15 @@ public class MasterClass {
         BinaryTreeImp tree = new BinaryTreeImp();
         Scanner sc = new Scanner(System.in);
         tree.populate(sc);
-//        tree.display();
-        tree.prettyDisplay();
+        tree.display();
+//        tree.prettyDisplay();
+//        String s1 = "anagram";
+//        String s2 = "nagaram";
+//        System.out.println(s2.contentEquals(s2));
+//        StringBuilder sb = new StringBuilder(s1);
+//        StringBuilder sb2 = new StringBuilder(s2);
+//        System.out.println(sb.equals(sb2));
+
     }
 
 }
