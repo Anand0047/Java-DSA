bc035c777f1802c9f2a1129b34404a3bd2ef8d06
Anand0047 <anandram221003@gmail.com>
2025-06-27 01:12:20 +0530

    INITIAL COMMIT

---
+++ src/LT_125.java
@@ -0,0 +1,23 @@
+public class LT_125 {
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println(  finf(s));
+    }
+    static boolean   finf(String s){
+
+        String news = s.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        int st = 0;
+        int end = news.length()-1;
+        boolean flag = true;
+        while(st<=end){
+            if(news.charAt(st) == news.charAt(end)){
+                st++;
+                end--;
+            }else {
+                flag = false;
+            }
+        }
+       return flag;
+        
+    }
+}
