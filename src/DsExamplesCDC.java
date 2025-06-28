58dfb9071c007a33be50ed89cf751eea0b3a5d56
Anand0047 <anandram221003@gmail.com>
2025-06-28 14:35:36 +0530

    INITIAL COMMIT

---
+++ src/DsExamplesCDC.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DsExamplesCDC {
+    public static void main(String[] args) {
+
+        Map<Integer,String> map = new HashMap<>();
+        map.put(1,"one");
+        map.put(2,"Two");
+        map.put(3,"three");
+        map.put(4,"four");
+
+        System.out.println(map.isEmpty());
+//        System.out.println((char) 65);
+    }
+}
