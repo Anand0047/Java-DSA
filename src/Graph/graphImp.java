5bcd4d3d375c75a927bfb6ed34037d2ffc2ffdbd
Anand0047 <anandram221003@gmail.com>
2025-10-09 00:36:46 +0530

    DAILY PRACTICE

--- src/Graph/graphImp.java
+++ src/Graph/graphImp.java
@@ -1,8 +1,49 @@
 package Graph;
+import java.util.*;
 
-public class graphImp {
+class Graph {
+    private int V; // number of vertices
+    private ArrayList<ArrayList<Integer>> adj; // adjacency list
+
+    Graph(int V) {
+        this.V = V;
+        adj = new ArrayList<>(V);
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+    }
+
+    void addEdge(int u, int v) {
+        adj.get(u).add(v);
+        adj.get(v).add(u); // remove this line for directed graph
+    }
+
+    void printGraph() {
+        for (int i = 0; i < V; i++) {
+            System.out.print(i + " -> ");
+            for (int node : adj.get(i)) {
+                System.out.print(node + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
+        Graph g = new Graph(5);
+        g.addEdge(0, 1);
+        g.addEdge(0, 4);
+        g.addEdge(1, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 4);
+        g.addEdge(2, 3);
+        g.addEdge(3, 4);
 
+        g.printGraph();
+    }
+}
+
+public  class graphImp {
+    public static void main(String[] args) {
 
     }
 }
