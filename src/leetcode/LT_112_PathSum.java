749f523dd56d3f5e9737dcd3df46ed82675e35e8
Anand0047 <anandram221003@gmail.com>
2025-10-24 14:06:02 +0530

    DAILY PRACTICE

---
+++ src/leetcode/LT_112_PathSum.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import com.sun.source.tree.Tree;
+
+import javax.swing.tree.TreeNode;
+
+//import static Graph.NumofProvincesGFG.dfs;
+
+public class LT_112_PathSum {
+    public static void main(String[] args) {
+
+    }
+//    public boolean hasPathSum(TreeNode root, int targetSum) {
+//        return dfs(root,root.val,targetSum);
+//    }
+//
+//    boolean dfs(TreeNode root , int current , int target){
+//        if(root == null) return ;
+//
+//        if(current == target){
+//            return true;
+//        }
+//
+//        int left = dfs(root.left, current + root.left.val , target);
+//        int right = dfs(root.right, current + root.right.val, target);
+//        return false;
+//    }
+
+}
